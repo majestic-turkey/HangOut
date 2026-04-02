afc390d8ec452d13e492444760cf85d7754102d0
A Majestic Turkey <ragingjockstrap@gmail.com>
2026-04-02 03:43:46 -0400

    Install socketio and integrate

--- server.ts
+++ server.ts
@@ -3,7 +3,6 @@
 */
 import express from 'express';
 import http from 'http';
-import path from 'node:path';
 import { Server } from 'socket.io';
 
 // Load environment variables from .env file and set constants
@@ -12,11 +11,17 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Derived constants
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-
 // Serve static elements
 app.use(express.static('public'));
+
+// On client connection
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected:', socket.id);
+    });
+});
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT || 3000}`);
